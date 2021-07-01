18c0f2a12d09c902d00c0ce8ebc85e0451b98397
Zachary Pavelski <zack88690@gmail.com>
2021-07-01 10:33:49 -0400

    NodeProduct Update

---
+++ NodeSample/routes.js
@@ -0,0 +1,18 @@
+
+exports.setRequestUrl=function(app){
+    var user = require('./controllers/user')
+        ,indexObj = require('./controllers/index');
+
+    app.get('/', user.login);
+    app.get('/dashboard', user.dashboard);
+    app.get('/inventory', user.inventory);
+    app.get('/additem', user.additem);
+    app.get('/scanner', user.scanner);
+    app.get('/account', user.account);
+    app.get('/signup', user.signup);
+
+
+  
+
+
+}
