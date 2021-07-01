1392d11ee87ee4b4794b51364e7109770f04d420
Zachary Pavelski <zack88690@gmail.com>
2021-07-01 12:27:31 -0400

    edited routes for sql

--- NodeSample/routes.js
+++ NodeSample/routes.js
@@ -14,7 +14,35 @@ exports.setRequestUrl=function(app){
     app.get('/switch', user.switch);
     app.get('/forgotPass', user.forgotPass);
     app.get('/thank', user.thank);
+    
+    app.post('/auth', function(request, response) {
+        var connection = mysql.createConnection({
+            host: 'pantry.cluster-cvskfciqfnj6.us-east-1.rds.amazonaws.com',
+            port: '3306',
+            user: 'pantryAdmin',
+            password: 'Pantry21!',
+            database: 'pantry'
+        });
+        var username = request.body.username;
+        var password = request.body.password;
+        if (username && password) {
+            connection.query('SELECT * FROM pantryUsers WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
+                if (results.length > 0) {
+                    request.session.loggedin = true;
+                    request.session.username = username;
+                    response.redirect('/');
+                } else {
+                    response.send('Incorrect Username and/or Password!');
+                }			
+                response.end();
+            });
+        } else {
+            response.send('Please enter Username and Password!');
+            response.end();
+        }
+    });
 
+  
 
   
 
