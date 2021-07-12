31cb101db23a6fa26812f10956abcce808da7261
Zachary Pavelski <zack88690@gmail.com>
2021-07-12 09:44:41 -0400

    Update routes.js

--- NodeSample/routes.js
+++ NodeSample/routes.js
@@ -1,4 +1,21 @@
 
+function hashPassword(password) {
+    var salt = crypto.randomBytes(128).toString('base64');
+    var iterations = 10000;
+    var hash = pbkdf2(password, salt, iterations);
+
+    return {
+        salt: salt,
+        hash: hash,
+        iterations: iterations
+    };
+}
+
+function isPasswordCorrect(savedHash, savedSalt, savedIterations, passwordAttempt) {
+    return savedHash == pbkdf2(passwordAttempt, savedSalt, savedIterations);
+}
+
+
 exports.setRequestUrl=function(app){
     var user = require('./controllers/user')
         ,indexObj = require('./controllers/index');
@@ -15,6 +32,36 @@ exports.setRequestUrl=function(app){
     app.get('/forgotPass', user.forgotPass);
     app.get('/thank', user.thank);
     
+    app.post('/createUser', function(request, response) {
+        var connection = mysql.createConnection({
+            host: 'pantry.cluster-cvskfciqfnj6.us-east-1.rds.amazonaws.com',
+            port: '3306',
+            user: 'pantryAdmin',
+            password: 'Pantry21!',
+            database: 'pantry'
+        });
+        /*
+        var username = document.getElementById('username').value;
+        var password = hashPassword(document.getElementById('password').value);
+        if (username && password) {
+            connection.query('SELECT * FROM pantryUsers WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
+                if (results.length > 0) {
+                   //REDIRECT TO DASHBOARD
+                   localStorage.setItem("name", "username");
+                   //localStorage.getItem("name");
+                   user.dashboard();
+                } else {
+                    response.send('Incorrect Username and/or Password!');
+                }			
+                response.end();
+            });
+        } else {
+            response.send('Please enter Username and Password!');
+            response.end();
+        }*/
+    });
+
+
     app.post('/auth', function(request, response) {
         var connection = mysql.createConnection({
             host: 'pantry.cluster-cvskfciqfnj6.us-east-1.rds.amazonaws.com',
@@ -23,14 +70,15 @@ exports.setRequestUrl=function(app){
             password: 'Pantry21!',
             database: 'pantry'
         });
-        var username = request.body.username;
-        var password = request.body.password;
+        var username = document.getElementById('username').value;
+        var password = hashPassword(document.getElementById('password').value);
         if (username && password) {
             connection.query('SELECT * FROM pantryUsers WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
                 if (results.length > 0) {
-                    request.session.loggedin = true;
-                    request.session.username = username;
-                    response.redirect('/dashboard');
+                   //REDIRECT TO DASHBOARD
+                   localStorage.setItem("name", "username");
+                   //localStorage.getItem("name");
+                   user.dashboard();
                 } else {
                     response.send('Incorrect Username and/or Password!');
                 }			
