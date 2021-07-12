46b6c76fde03d7e51936c22c2e312bc1a18295b2
Zachary Pavelski <zack88690@gmail.com>
2021-07-12 11:37:38 -0400

    creating Users

--- NodeSample/routes.js
+++ NodeSample/routes.js
@@ -1,3 +1,4 @@
+var mysql = require('mysql');
 
 function hashPassword(password) {
     var salt = crypto.randomBytes(128).toString('base64');
@@ -32,7 +33,7 @@ exports.setRequestUrl=function(app){
     app.get('/forgotPass', user.forgotPass);
     app.get('/thank', user.thank);
     
-    app.post('/createUser', function(request, response) {
+    app.get('/createUser', function(request, response) {
         var connection = mysql.createConnection({
             host: 'pantry.cluster-cvskfciqfnj6.us-east-1.rds.amazonaws.com',
             port: '3306',
@@ -40,33 +41,32 @@ exports.setRequestUrl=function(app){
             password: 'Pantry21!',
             database: 'pantry'
         });
-        var username = document.getElementById('username').value;
-        var password = hashPassword(document.getElementById('password').value);
-        var email = document.getElementById('email').value;
-        var zipCode = document.getElementById('zipCode').value;
-        var numPeople = document.getElementById('numPeople').value;
-        /*
+        connection.connect(function(err){
+            if(err){
+                console.error('Db connection failed: ' + err.stack);
+                return;
+            }
+            console.log('Connected!');
+        });
         
-        if (username && password) {
-            connection.query('SELECT * FROM pantryUsers WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
-                if (results.length > 0) {
-                   //REDIRECT TO DASHBOARD
-                   localStorage.setItem("name", "username");
-                   //localStorage.getItem("name");
-                   user.dashboard();
-                } else {
-                    response.send('Incorrect Username and/or Password!');
-                }			
-                response.end();
-            });
-        } else {
-            response.send('Please enter Username and Password!');
-            response.end();
-        }*/
+        var username = request.query.username;
+        var fullName = request.query.fullName;
+        var password = request.query.password;
+        var email = request.query.email;
+        var zipCode = request.query.zipCode;
+        var numPeople = request.query.numPeople;
+        var minors = request.query.minors;
+        //var sql = `INSERT INTO scoutingDataTab (compId, teleop_cargoTot, teleop_hatchTot) VALUES (${compId}, ${teleop_noOfCargo}, ${teleop_noOfHatch})`;
+        var sql = `INSERT INTO pantryUsers (email, fullName, password, username, zipcode, numPeople, numMinors) VALUES (${email}, ${fullName}, ${password}, ${username}, ${zipCode}, ${numPeople}, ${minors})`;   
+        /*connection.query(sql, function (err, result) {
+            if (err) throw err;
+            console("1 record inserted");
+          });   */
+        user.dashboard;
     });
 
 
-    app.post('/auth', function(request, response) {
+    app.get('/auth', function(request, response) {
         var connection = mysql.createConnection({
             host: 'pantry.cluster-cvskfciqfnj6.us-east-1.rds.amazonaws.com',
             port: '3306',
@@ -74,6 +74,14 @@ exports.setRequestUrl=function(app){
             password: 'Pantry21!',
             database: 'pantry'
         });
+        connection.connect(function(err){
+            if(err){
+                console.error('Db connection failed: ' + err.stack);
+                return;
+            }
+            console.log('Connected!');
+        });
+        
         var username = document.getElementById('username').value;
         var password = hashPassword(document.getElementById('password').value);
         if (username && password) {
@@ -82,7 +90,7 @@ exports.setRequestUrl=function(app){
                    //REDIRECT TO DASHBOARD
                    localStorage.setItem("name", "username");
                    //localStorage.getItem("name");
-                   user.dashboard();
+                   user.dashboard;
                 } else {
                     response.send('Incorrect Username and/or Password!');
                 }			
