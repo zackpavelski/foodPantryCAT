90c8f90964e60ae42a20ddbc787587c1a9ae6597
coooolart <acdriiska111@gmail.com>
2021-07-28 10:51:32 -0400

    Tables Created
    Table Data, and merging test to work with routes

--- test/app.js
+++ test/app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var urlHelper = require("./routes.js")
+, http = require('http')
+, fs = require('fs')
+, config = require("./config")
+, mysql = require('mysql')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +15,15 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(app.router);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -26,6 +32,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+urlHelper.setRequestUrl(app);
 
 // error handler
 app.use(function(err, req, res, next) {
