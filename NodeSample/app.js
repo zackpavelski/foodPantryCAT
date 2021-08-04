464c5e052a8f301e9c9edcf4c75773fb2f0e139c
Zachary Pavelski <zack88690@gmail.com>
2021-08-04 12:03:42 -0400

    change to express v4.17.1

--- NodeSample/app.js
+++ NodeSample/app.js
@@ -4,33 +4,29 @@ var express = require('express')
   , fs = require('fs')
  , config = require("./config")
  , mysql = require('mysql')
+ , methodOverride = require('method-override')
+ , logger = require('morgan')
+ , bodyParser = require('body-parser')
   , path = require('path');
 
 var app = express();
 
 app.engine('html', require('ejs').renderFile);
 
-app.configure(function(){
-  app.set('port', config.port);
-  app.set('view engine', 'ejs');
-  app.set('views', __dirname + '/views');
 
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.set('port', config.port);
+app.set('view engine', 'ejs');
 
-});
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+//app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+
+
 
 urlHelper.setRequestUrl(app);
-
-app.on('close', function(errno) {
-	//ongoHelper.disconnect(function(err) { });
-});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
